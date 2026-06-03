2c9ea50d3d8b544a2ec2358ee277f727811d3ed4
Origogi <rlawjdxo1212@naver.com>
2026-06-03 23:18:46 +0900

    Configure ADK LiteLLM agents

--- a2a/user-facing-agent/user-facing-agent/agent.py
+++ a2a/user-facing-agent/user-facing-agent/agent.py
@@ -1,1 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+
+root_agent = Agent(
+    name="StudentHelperAgent",
+    description="An agent that can helps students with their homework",
+    model=LiteLlm("openai/gpt-4.1-mini"),
+    sub_agents=[],
+)
