72a3f7b13ce1b1ff29eb6f4a600c05901b8c7bce
Origogi <rlawjdxo1212@naver.com>
2026-06-06 23:09:25 +0900

    Complete RemoteA2aAgent

--- a2a/remote_adk_agent/agent.py
+++ a2a/remote_adk_agent/agent.py
@@ -6,11 +6,20 @@ from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
 
+def dummy_tool(hello:str):
+    """
+    Dummy tool. helps the agent
+    """
+    return "world"
+
 agent = Agent(
     name="HistoryHelperAgent",
     description="An agent that can helps students with their history homework",
     model=LiteLlm("openai/gpt-4.1-mini"),
     sub_agents=[],
+    tools=[
+        dummy_tool
+    ]
 )
 
 app = to_a2a(agent, port=8001)
